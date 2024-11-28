eec910a95464a603cd9be25202bf2fccf63ab590
0xCamiX <juancamilogallego70@icloud.com>
2024-11-27 22:25:46 -0500

    Add simple authentication

--- app/src/main/java/com/camix/cop16connect/view/MainActivity.java
+++ app/src/main/java/com/camix/cop16connect/view/MainActivity.java
@@ -1,5 +1,7 @@
 package com.camix.cop16connect.view;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences preferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        boolean isLoggedIn = preferences.getBoolean("is_logged_in", false);
+
+        if (!isLoggedIn) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
