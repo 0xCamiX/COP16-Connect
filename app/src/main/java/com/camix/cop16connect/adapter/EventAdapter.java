84b0e31ebaa67a7e6c5a3060e0252a343e7ff535
0xCamiX <juancamilogallego70@icloud.com>
2024-11-28 18:31:37 -0500

    Fix null item_event_activity

--- app/src/main/java/com/camix/cop16connect/adapter/EventAdapter.java
+++ app/src/main/java/com/camix/cop16connect/adapter/EventAdapter.java
@@ -14,6 +14,7 @@ import com.camix.cop16connect.R;
 import com.camix.cop16connect.model.Event;
 import com.camix.cop16connect.model.Location;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventAdapter extends ArrayAdapter<Event> {
@@ -25,34 +26,47 @@ public class EventAdapter extends ArrayAdapter<Event> {
     public EventAdapter(@NonNull Context context, @NonNull List<Event> events, @NonNull List<Location> locations) {
         super(context, 0, events);
         this.context = context;
-        this.events = events;
-        this.locations = locations;
+        this.events = events != null ? events : new ArrayList<>();
+        this.locations = locations != null ? locations : new ArrayList<>();
+    }
+
+    private static class ViewHolder {
+        TextView tvEventTitle;
+        TextView tvEventDescription;
+        TextView tvEventStartDate;
+        TextView tvEventEndDate;
+        TextView tvEventLocation;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.item_event, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.tvEventTitle = convertView.findViewById(R.id.tv_event_title);
+            viewHolder.tvEventDescription = convertView.findViewById(R.id.tv_event_description);
+            viewHolder.tvEventStartDate = convertView.findViewById(R.id.tv_event_start_date);
+            viewHolder.tvEventEndDate = convertView.findViewById(R.id.tv_event_end_date);
+            viewHolder.tvEventLocation = convertView.findViewById(R.id.tv_event_location);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
         Event event = events.get(position);
 
-        TextView tvEventTitle = convertView.findViewById(R.id.tv_event_title);
-        TextView tvEventDescription = convertView.findViewById(R.id.tv_event_description);
-        TextView tvEventStartDate = convertView.findViewById(R.id.tv_event_start_date);
-        TextView tvEventEndDate = convertView.findViewById(R.id.tv_event_end_date);
-        TextView tvEventLocation = convertView.findViewById(R.id.tv_event_location);
-
-        tvEventTitle.setText(event.getTitle());
-        tvEventDescription.setText(event.getDescription());
-        tvEventStartDate.setText(event.getStartDate());
-        tvEventEndDate.setText(event.getEndDate());
+        viewHolder.tvEventTitle.setText(event.getTitle());
+        viewHolder.tvEventDescription.setText(event.getDescription());
+        viewHolder.tvEventStartDate.setText(event.getStartDate());
+        viewHolder.tvEventEndDate.setText(event.getEndDate());
 
         // Find the location by ID and set the location name and ID
         for (Location location : locations) {
             if (location.getId() == Integer.parseInt(event.getLocationId())) {
-                tvEventLocation.setText(location.getName() + " (" + location.getId() + ")");
+                viewHolder.tvEventLocation.setText(location.getName() + " (" + location.getId() + ")");
                 break;
             }
         }
