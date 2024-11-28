70330c7de986d60cb9c9a010112684fc917800b0
0xCamiX <juancamilogallego70@icloud.com>
2024-11-27 22:33:52 -0500

    Add view for admin activity

--- app/src/main/java/com/camix/cop16connect/view/AdminActivity.java
+++ app/src/main/java/com/camix/cop16connect/view/AdminActivity.java
@@ -1,4 +1,13 @@
 package com.camix.cop16connect.view;
 
-public class AdminActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.camix.cop16connect.R;
+
+public class AdminActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
 }
